4033a4c1713bcfc3db89d714957c575972623700
RajatMCAF <rajat.dahiya@mcaf.nb.ca>
2023-10-18 10:24:30 -0700

    [ADD] Skeleton Added
    Basic Structure

--- resource_management/models/models.py
+++ resource_management/models/models.py
@@ -1,16 +1,8 @@
-# from odoo import models, fields, api
+from odoo import models, fields
 
 
-# class resource_management(models.Model):
-#     _name = 'resource_management.resource_management'
-#     _description = 'resource_management.resource_management'
+class ResourceManagement(models.Model):
+    _name= 'resource.management'
+    _description = 'Resource Management'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
