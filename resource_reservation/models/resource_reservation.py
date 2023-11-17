2286d91325f2fee0f6dbeb3c7180537eba430e09
andrii <andrii.koloskov@mcaf.nb.ca>
2023-11-17 07:32:34 -0800

    have problems with resource_reservation create

--- resource_reservation/models/resource_reservation.py
+++ resource_reservation/models/resource_reservation.py
@@ -33,8 +33,14 @@ class ResourceReservation(models.Model):
     _description = 'Resource Reservation'
 
     title = fields.Char(string='Title ', required=True)
-    name = fields.Many2one('resource',
-                           string="Resource Name", required=True)
+    name = fields.Many2one(
+        'resource.detail',
+        string='Resource Name',
+        options={'no_create': True})
+    resource_type = fields.Many2one(
+        'resource.type',
+        string='Resource Type',
+        options={'no_create': True})
     start_datetime = fields.Datetime(string='Start Date & Time',
                                      default=lambda self:
                                      fields.Datetime.now(),
@@ -68,6 +74,11 @@ class ResourceReservation(models.Model):
         string="Reservation Tag",
         required=True,
         widget='many2many_tags')
+
+    color_reserv = fields.Integer(
+        string='Color',
+        related='resource_type.color_resource_type',
+        store=True)
 
     def update_booking_status_cancel(self):
         self.write({'booking_status': 'cancelled'})
@@ -109,3 +120,15 @@ class ResourceReservation(models.Model):
                 raise exceptions.ValidationError(_("Bookings for "
                                                    "past dates are "
                                                    "not allowed."))
+
+    @api.onchange('resource_type')
+    def _onchange_resource_type(self):
+        if self.resource_type:
+            # Filter the available name options based on the selected resource_type
+            return {'domain': {'name': [('resource_type', '=', self.resource_type.id), ('id', '!=', False)]}}
+
+    @api.onchange('name')
+    def _onchange_name(self):
+        if self.name:
+            # Filter the available resource_type options based on the selected name
+            return {'domain': {'resource_type': [('id', '=', self.name.resource_type.id), ('id', '!=', False)]}}
