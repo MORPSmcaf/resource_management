189bd58c5d4dc5cc935944a9f51fda5f91bd1b13
RajatMCAF <rajat.dahiya@mcaf.nb.ca>
2023-12-04 11:12:48 -0800

    [IMP] message for access error
    - removed record rules

--- resource_reservation/__manifest__.py
+++ resource_reservation/__manifest__.py
@@ -12,7 +12,6 @@
     'data': [
         'security/resource_reservation_groups.xml',
         'security/ir.model.access.csv',
-        'security/resource_reservation_rules.xml',
         'security/resource_reservation_security.xml',
         'views/resource_reservation_views.xml',
         'views/resource_reservation_tag_views.xml',
