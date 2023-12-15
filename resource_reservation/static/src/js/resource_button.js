a9b75de7a27bcbacbea1a3b9a67b586932f48818
mcaf <rajat.dahiya@mcaf.nb.ca>
2023-12-15 10:44:20 -0800

    [FIX] view

--- resource_reservation/static/src/js/resource_button.js
+++ resource_reservation/static/src/js/resource_button.js
@@ -6,31 +6,17 @@ export class ResourceListController extends ListController {
    setup() {
        super.setup();
    }
-OnTestClick() {
-       // Check if the user belongs to the required group
-       const isGroupUser = this.env.services.rpc({
-           model: 'res.users',
-           method: 'has_group',
-           args: ['resource_reservation.group_resource_admin'],
-       });
-
-       if (isGroupUser) {
-           // User belongs to the group, proceed with the action
-           console.log("I am here")
-           this.actionService.doAction({
-              type: 'ir.actions.act_window',
-              name: 'Resource Reservations - Pivot View',
-              res_model: 'resource.reservation',
-              view_mode: 'pivot',
-              view_type: 'pivot',
-              views: [[false, 'pivot']],
-              target: 'current',
-              res_id: false,
-          });
-       } else {
-           // User does not belong to the group, raise an access error
-           throw new AccessError(_t("Access Denied. You do not have the required group."));
-       }
+   OnTestClick() {
+       this.actionService.doAction({
+          type: 'ir.actions.act_window',
+          name: 'Resource Reservations - Pivot View',
+          res_model: 'resource.reservation',
+          view_mode: 'pivot',
+          view_type: 'pivot',
+          views: [[false, 'pivot']],
+          target: 'current',
+          res_id: false,
+      });
    }
 }
 registry.category("views").add("button_in_tree", {
